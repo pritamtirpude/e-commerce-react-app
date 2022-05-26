4d65118bc2b1141260e63b0c88c104a6c6de11bd
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2022-05-26 17:49:15 +0530

    fix category container responsiveness on mobile

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -5,6 +5,11 @@ export const CategoryContainer = styled.div`
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+    row-gap: 25px;
+  }
 `;
 
 export const CategoryTitle = styled.h2`
